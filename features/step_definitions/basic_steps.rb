c534c7f17fd7459fe7f7efc6bd9497430d9609b1
Carissa <carissa@edgren.nu>
2017-08-01 12:12:08 +0200

    Setup a route and controller for landing page

--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -1,5 +1,5 @@
-When(/^I visit "([^"]*)" page$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
+When(/^I visit "([^"]*)" page$/) do |page_name|
+  visit root_path
 end
 
 Then(/^I should be on "([^"]*)" page$/) do |arg1|
