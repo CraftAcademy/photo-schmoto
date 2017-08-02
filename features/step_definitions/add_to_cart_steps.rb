c396927aa609c8b2c026fc9f5c10dcc4c9ed7f85
j0sv <svensson.jonas@gmail.com>
2017-08-02 10:49:56 +0200

    Add feature test for Add to cart and step definitions

---
+++ features/step_definitions/add_to_cart_steps.rb
@@ -0,0 +1,7 @@
+When(/^I add "([^"]*)" to cart$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^Items in cart should be "(\d+)$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
