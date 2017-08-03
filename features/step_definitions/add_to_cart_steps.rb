fde06a7dce3ba3983cc36f73fb8511b76f72aff1
j0sv <svensson.jonas@gmail.com>
2017-08-03 10:47:33 +0200

    WIP test to find cart count

--- features/step_definitions/add_to_cart_steps.rb
+++ features/step_definitions/add_to_cart_steps.rb
@@ -3,5 +3,9 @@ When(/^I add "([^"]*)" to cart$/) do |photo_title|
   click_link_or_button add_button_with_id
 end
 
-Then(/^Items in cart should be "(\d+)$/) do |arg1|
+Then(/^Items in cart should be "([^"]*)"$/) do |expected_count|
+  within "#cart_item" do
+    page.should have_content expected_count
+  end
 end
+
