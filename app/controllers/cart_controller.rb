e02629c7adeea2cb50d96a99ea48e5b62837e7ae
j0sv <svensson.jonas@gmail.com>
2017-08-04 10:18:50 +0200

    Corrected issues after review

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -5,12 +5,15 @@ class CartController < ApplicationController
     else
       cart = Cart.create
     end
+
     photo = Photo.find(params[:photo_id])
+
     if cart.add(photo, photo.price)
       flash[:notice] = photo.title + " was  successfully added to cart!"
     else
       flash[:alert] = "Item not added, try again!"
     end
+
     redirect_to photos_path
   end
 end
