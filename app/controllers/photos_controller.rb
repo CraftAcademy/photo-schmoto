da09796887b0f5e24cfdbc40e59ec7111f644228
j0sv <svensson.jonas@gmail.com>
2017-08-04 07:21:09 +0200

    Move cart item count from photo to layout and application_helper

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,8 +1,5 @@
 class PhotosController < ApplicationController
   def index
     @photos = Photo.all
-    if Cart.last
-      @cart = Cart.last.total_unique_items || 0
-    end
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def get_cart_items_count
+    if Cart.last
+      Cart.last.total_unique_items || 0
+    end
+  end
+
 end
